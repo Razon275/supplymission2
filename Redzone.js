d98d60e97533384a8c33028cbc54adb3f529bb11
Razon275 <73532970+Razon275@users.noreply.github.com>
2020-11-24 19:16:41 +0530

    Add files via upload

---
+++ Redzone.js
@@ -0,0 +1,11 @@
+class Redzone{
+    constructor(x,y,width,height){
+        
+       this.body=Bodies.rectangle(x,y,width,height);
+       this.width=width;
+       this.height=height;
+       World.add(world,this.body);
+
+    }
+    
+}
